92ce211bb9364576324528d325714d3134e231a5
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-05-15 01:22:16 +0300

    fixed burger menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,8 +1,11 @@
 const menu = () => {
     const clubsList = document.querySelector('.clubs-list'),
         widthWindow = document.documentElement.clientWidth,
+        heightWindow = document.documentElement.clientHeight,
         menuButton = document.querySelector('.hidden-large'),
-        menuList = document.querySelector('.hidden-small');
+        menuList = document.querySelector('.hidden-small'),
+        topMenu = document.querySelector('.top-menu');
+        
 
     // Выбрать клуб-выпадающее меню
     document.addEventListener('click', event => {
@@ -14,10 +17,18 @@ const menu = () => {
         }
     });
 
+    // скрываем меню и добавляем бургер меню
     window.addEventListener('resize', () => {
         if (widthWindow < 768) {
             menuButton.style.display = 'block';
             menuList.style.display = 'none';
+        }
+    });
+
+    window.addEventListener('scroll', () => {
+        if (pageYOffset > heightWindow) {
+            topMenu.style.cssText = `position: fixed; `;
+            document.body.style.cssText = 'margin-top: 60px';
         }
     });
     
