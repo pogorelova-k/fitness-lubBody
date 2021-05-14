166da0e8fec8052ebfb391bc6408de700ddce90e
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-05-14 23:23:29 +0300

    add galery slider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@
 import menu from "./modules/menu";
 import popup from "./modules/popup";
 import sendForm from "./modules/sendForm";
-import slider from "./modules/slider";
+import mainSlider from "./modules/mainSlider";
 import validator from "./modules/validation";
+import SliderCarousel from './modules/sliderCarousel';
+import sliderGallery from "./modules/sliderGallery";
+
+const carousel = new SliderCarousel({
+    main: '.services-wrapper',
+    wrap: '.services-slider',
+    slide: '.services-slide',
+    slidesShow: 5,
+    next: '.carousel-right',
+    prev: '.carousel-left',
+    infinite: false,
+    responsive: [{
+        breakpoint: 1280,
+        slidesShow: 4
+    },{
+        breakpoint: 1024,
+        slidesShow: 3
+    },
+    {
+        breakpoint: 768,
+        slidesShow: 2
+    },
+    {
+        breakpoint: 576,
+        slidesShow: 1
+    }],
+});
 
 menu();
 popup();
 sendForm();
 validator();
-slider();
+mainSlider();
+carousel.init();
+sliderGallery();
---
+++ src/modules/sliderGallery.js
@@ -0,0 +1,102 @@
+const sliderGallery = () => {
+    const slider = document.querySelector('.gallery-slider'),	
+        slide = slider.querySelectorAll('.slide'),
+        dots = document.querySelector('.portfolio-dots');
+
+    //Текущий номер слайда
+    let currentSlide = 0,
+        interval;
+
+    slide.forEach(() => {
+        const li = document.createElement('li');
+        li.classList.add('dot');
+        dots.append(li);
+        dots.firstChild.classList.add('dot-active');
+    });
+
+    const	dot = document.querySelectorAll('.dot');
+
+    const prevSlide = (elem, index, strClass) => {
+        elem[index].classList.remove(strClass);
+    };
+
+    const nextSlide = (elem, index, strClass) => {
+        elem[index].classList.add(strClass);
+    };
+
+    // автоматическое перелистывание слайдера autoPlay
+    const autoPlaySlide = () => {
+        prevSlide(slide, currentSlide, 'active');
+        // prevSlide(dot, currentSlide, 'dot-active');
+        currentSlide++;
+        if (currentSlide >= slide.length) {
+            currentSlide = 0;
+        }
+        nextSlide(slide, currentSlide, 'active');
+        // nextSlide(dot, currentSlide, 'dot-active');
+    };
+
+    // Запуск слайдера
+    const startSlide = (time = 3000) => {
+        interval = setInterval(autoPlaySlide, time);
+    };
+
+    // Остановка слайдера
+    const stopSlide = () => {
+        clearInterval(interval);
+    };
+
+    slider.addEventListener('click', event => {
+        event.preventDefault();
+
+        const target = event.target;
+
+        if (!target.matches('.portfolio-btn, .dot')) {
+            return;
+        }
+
+        prevSlide(slide, currentSlide, 'active');
+        prevSlide(dot, currentSlide, 'dot-active');
+
+        if (target.matches('.gallery-next')) {
+            currentSlide++;
+        } else if (target.matches('.gallery-prev')) {
+            currentSlide--;
+        } else if (target.matches('.dot')) {
+            dot.forEach((elem, index) => {
+                if (elem === target) {
+                    currentSlide = index;
+                }
+            });
+        }
+
+        if (currentSlide >= slide.length) {
+            currentSlide = 0;
+        }
+
+        if (currentSlide < 0) {
+            currentSlide = slide.length - 1;
+        }
+
+        nextSlide(slide, currentSlide, 'active');
+        nextSlide(dot, currentSlide, 'dot-active');
+    });
+
+    slider.addEventListener('mouseover', event => {
+        if (event.target.matches('.gallery-btn') ||
+        event.target.matches('.dot')) {
+            stopSlide();
+        }
+    });
+
+    slider.addEventListener('mouseout', event => {
+        if (event.target.matches('.gallery-btn') ||
+        event.target.matches('.dot')) {
+            startSlide();
+        }
+    });
+
+    startSlide(1500);
+};
+
+export default sliderGallery;
