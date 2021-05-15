ec08e7d12333979f3e6c855d257df8b6f0b5c006
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-05-15 13:04:55 +0300

    display button arrow up when scrolling

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,11 +1,13 @@
 const menu = () => {
     const clubsList = document.querySelector('.clubs-list'),
         widthWindow = document.documentElement.clientWidth,
-        heightWindow = document.documentElement.clientHeight,
         menuButton = document.querySelector('.hidden-large'),
         menuList = document.querySelector('.hidden-small'),
         topMenu = document.querySelector('.top-menu'),
-        popupMenu = document.querySelector('.popup-menu');
+        popupMenu = document.querySelector('.popup-menu'),
+        topUp = document.getElementById('totop');
+
+    totop.style.display = 'none';
 
     // Выбрать клуб-выпадающее меню
     document.addEventListener('click', event => {
@@ -25,19 +27,25 @@ const menu = () => {
         }
     });
 
+    // скрол на один высоту первого блока
     window.addEventListener('scroll', () => {
-        if (pageYOffset > heightWindow) {
+        if (pageYOffset > 700) {
+            // фиксируем меню
             topMenu.style.cssText = `position: fixed; `;
             document.body.style.cssText = 'margin-top: 60px';
+            // добавляем кнопку top up
+            totop.style.display = 'block';
+        } else {
+            totop.style.display = 'none';
         }
     });
 
+    // открытие и закрытие меню
     menuButton.addEventListener('click', () => {
         popupMenu.style.display = 'flex';
 
         popupMenu.addEventListener('click', event => {
             const target = event.target;
-            console.log(target);
             if (target.closest('.close-menu-btn') || target.closest('.scroll')) {
                 popupMenu.style.display = 'none';
             }
